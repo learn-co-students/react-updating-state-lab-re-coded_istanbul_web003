7d25b860657bc2dc12b4e55809c09cc8390afbb8
orcunsagirsoy <orcunsagirsoy@gmail.com>
2020-10-24 13:31:24 +0300

    Done.

--- src/components/DigitalClicker.js
+++ src/components/DigitalClicker.js
@@ -1,1 +1,26 @@
 // Code DigitalClicker Component Here
+import React, { Component } from 'react'
+
+export default class DigitalClicker extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            timesClicked: 0,
+        }
+    }
+    handleClick = (e)=> {
+        this.setState(previousState => {
+              return {
+                  timesClicked: previousState.timesClicked + 1
+              };
+        })
+    }
+    render() {
+        return (
+            <div>
+                <button className='bitrate' onClick={this.handleClick}>{this.state.timesClicked}</button>
+            </div>
+        )
+    }
+}
